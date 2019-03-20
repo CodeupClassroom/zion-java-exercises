2e99b8973ea5f2971fc2b5a6a8bb6690211ee2d3
Daniel Fryar <daniel.fryar@gmail.com>
2019-03-20 14:32:26 -0500

    Console IO lesson

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] banana) {
-        System.out.print("Hello");
-        System.out.print(" ");
-        System.out.print("Zion!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.print("What is your favorite number? ");
+        int favoriteNumber = scanner.nextInt();
+
+        System.out.format("Hello " + name + ". Your favorite number is %d", favoriteNumber);
     }
 }
