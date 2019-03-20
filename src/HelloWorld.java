6b8c76b8a93bbda83bbfdbc6a346a6e15d345447
Daniel Fryar <daniel.fryar@gmail.com>
2019-03-20 09:10:43 -0500

    Syntax Lesson

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,39 @@
 public class HelloWorld {
-    public static void main(String[] banana) {
-        System.out.print("Hello");
-        System.out.print(" ");
-        System.out.print("Zion!");
+    public static void main(String[] args) {
+        // boolean *
+        // byte
+        // short
+        // int *
+        // long
+        // float
+        // double *
+        // char
+        // + - * / %
+
+        // 0000 1000
+        // 0000 1001
+        // 0111 1111 1111 1111 1111 1111 1111 1111
+        // 1000 0000 0000 0000 0000 0000 0000 0000
+
+        byte littleNum = 127;
+        int biggerNum = littleNum;
+        float imprecisePi = 3.14f;
+        System.out.println(imprecisePi);
+        final double PI = 3.14159212345;
+//        PI += 7; error
+        System.out.println(PI);
+//        int almostPi = 3;
+//        pi = almostPi;
+
+        char letter = 'a';
+        char letterb = 'b';
+//        letter++;
+        System.out.println("" + letter + letterb);
+
+        biggerNum = 2147483647;
+        biggerNum++;
+        System.out.println(biggerNum);
+        biggerNum--;
+        System.out.println(biggerNum);
     }
 }
