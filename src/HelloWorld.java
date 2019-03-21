82b831f05130099566082b89702a5b09697fb14e
Daniel Fryar <daniel.fryar@gmail.com>
2019-03-21 14:31:14 -0500

    hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,18 +3,13 @@ import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        byte num1 = 100;
-        int num2 = 100;
-        if (num1 == num2) {
-            System.out.println("Hey look, they're equal!");
-        } else {
-            System.out.println("They are not equal.");
-        }
-        char letter = '!';
-        for (int i = Character.valueOf(letter);i < Character.valueOf('z'); i++) {
-            System.out.format("%d = '%s'%n",i,letter);
-            letter++;
-        }
 
+       for (int i=1;true;i++) {
+           if (i>10) break;
+           if (i%2!=0) {
+               continue;
+           }
+           System.out.println(i);
+       }
     }
 }
