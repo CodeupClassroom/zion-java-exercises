99815468fd9eca1b8150aac973f27715974d9803
Daniel Fryar <daniel.fryar@gmail.com>
2019-03-28 09:47:25 -0500

    Finished exercises

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,26 +1,47 @@
 package movies;
 import util.*;
 
+import java.util.Arrays;
+
 public class MoviesApplication {
 
+    static Movie[] movies;
+    static Input input;
+
     public static void viewAllMovies() {
-        Movie[] movies = MoviesArray.findAll();
+//        Movie[] movies = MoviesArray.findAll();
         for (Movie movie : movies) {
             System.out.println(movie.getName());
         }
+        System.out.println();
     }
 
     public static void viewMoviesByCategory(String category) {
-        Movie[] movies = MoviesArray.findAll();
+//        Movie[] movies = MoviesArray.findAll();
         for (Movie movie : movies) {
             if (movie.getCategory().equals(category))
                 System.out.println(movie.getName());
         }
+        System.out.println();
+    }
+
+    public static void addMovie() {
+        System.out.println("What is the title?");
+        String whatIsGoingOn = input.getString();
+        String title = input.getString();
+        System.out.println("What category is this movie in?");
+        String category = input.getString();
+        Movie[] newMovies = Arrays.copyOf(movies, movies.length+1);
+
+        newMovies[newMovies.length - 1] = new Movie(title, category);
+
+        movies = newMovies;
 
     }
 
     public static void main(String[] args) {
-
+        movies = MoviesArray.findAll();
+        input = new Input();
 
         while (true) {
             System.out.printf("What would you like to do?\n" +
@@ -30,14 +51,15 @@ public class MoviesApplication {
                 "2 - view movies in the animated category\n" +
                 "3 - view movies in the drama category\n" +
                 "4 - view movies in the horror category\n" +
-                "5 - view movies in the scifi category\n");
+                "5 - view movies in the scifi category\n" +
+                "6 - add movie\n");
 
-            Input input = new Input();
             int choice = input.getInt();
 
             switch (choice) {
                 case 0:
-                    break;
+                    return;
+//                    break;
                 case 1:
                     viewAllMovies();
                     break;
@@ -53,14 +75,17 @@ public class MoviesApplication {
                 case 5:
                     viewMoviesByCategory("scifi");
                     break;
+                case 6:
+                    addMovie();
+                    break;
                 default:
                     break;
             }
 
 
-            System.out.println("Would you like to continue? (y/n)");
-            if (input.yesNo()) continue;
-            else break;
+//            System.out.println("Would you like to continue? (y/n)");
+//            if (input.yesNo()) continue;
+//            else break;
         }
     }
 }
