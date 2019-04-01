2a184edc177000820eef2079966e7ea84a78a281
Daniel Fryar <daniel.fryar@gmail.com>
2019-04-01 14:09:53 -0500

    HashMap

--- src/oop/CollectionsPractice.java
+++ src/oop/CollectionsPractice.java
@@ -3,50 +3,26 @@ package oop;
 import shapes.Shape;
 import shapes.Square;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CollectionsPractice {
 
     public static void main(String[] args) {
-        String[] stringArray = new String[10];
-        List<String> stringCollection = new ArrayList<>();
 
-        System.out.println(stringCollection.isEmpty());
-        List<Square> squares;
-        List<Shape> shapes;
+        Map<Integer, String> friends = new HashMap<>();
 
-        stringArray[0] = "Hello";
-//        stringCollection.get(i);
-        stringCollection.add("Hello");
-        System.out.println(stringCollection.isEmpty());
-        stringCollection.add("Hello");
-        stringCollection.add("Hello");
-        stringCollection.add("Hello");
-        stringCollection.add("Hello");
-        stringCollection.add(2,"World");
-        stringCollection.add(3,"Codeup");
-        System.out.println(stringCollection);
-
-        for (String str : stringCollection) {
-            System.out.println(str);
-
-        }
-        System.out.println(stringCollection.contains("World"));
-        stringCollection.remove("World");
-        System.out.println(stringCollection.contains("World"));
-        System.out.println(stringCollection);
-
-        stringCollection.clear();
-        System.out.println(stringCollection.isEmpty());
-
-
-        List<Student> zion = new ArrayList<>();
-        zion.add(new Student("student1"));
-        zion.add(new Student("student2"));
-
-        List<Student> zion2 = new ArrayList<>();
-        zion2.addAll(zion);
+        friends.put(78, "Fred");
+        friends.put(95,"Macaroni");
+        friends.put(55, "Pasta");
+        friends.replace(55, "Fettuccine");
+        System.out.println(friends.get(95));
+        System.out.println(friends);
+        if (friends.containsKey(55))
+            System.out.println("friend 55 is " + friends.get(55));
+        friends.remove(88);
+        System.out.println(friends);
+        friends.clear();
+        System.out.println(friends);
 
     }
 
