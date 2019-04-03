45c7ca392aefb25cddfeea8261819c2e214c2bd5
Daniel Fryar <daniel.fryar@gmail.com>
2019-04-03 09:20:30 -0500

    Exceptions Lesson

--- src/oop/ExceptionsSandbox.java
+++ src/oop/ExceptionsSandbox.java
@@ -1,7 +1,16 @@
 package oop;
 
+class CustomException extends Exception {}
+
 public class ExceptionsSandbox {
 
+
+    public static void dangerMethod() throws CustomException {
+        System.out.printf("Aaaah!!");
+//        throw new CustomException();
+        String[] words = {"one", "two", "three"};
+        System.out.println(words[1000]);
+    }
     public static void main(String[] args) {
         System.out.printf("Here we are at the start of our program!%n");
 
@@ -9,9 +18,10 @@ public class ExceptionsSandbox {
         int i = 3;
 
         try {
+//            throw new Exception("oops");
 //            if (i < 0 || i >= words.length) {
 //                // array index is out of bounds!!
-//                throw new Exception("Array index was erroneous!!!");
+//                throw new CustomException();
 //            }
             System.out.printf("Here we are in the danger zone!!%n");
             System.out.printf(words[i]);
@@ -23,7 +33,25 @@ public class ExceptionsSandbox {
         catch (Exception e) {
             System.out.printf("We are catching our own exception%n");
             System.out.println(e);
+        } finally {
+            System.out.printf("We should always get here%n");
         }
+
+        try {
+            dangerMethod();
+        } catch (Exception ce) {
+            System.out.printf("We caught something");
+        }
+
+        int d = 0;
+        int n = 20;
+        try {
+            int fraction = n / d;
+            System.out.println("This line will not be Executed");
+        } catch (ArithmeticException e) {
+            System.out.println("In the catch Block due to Exception = " + e);
+        }
+        System.out.println("End Of Main");
 
         System.out.println("Here we are at the end of our program!");
     }
